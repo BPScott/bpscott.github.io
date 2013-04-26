0e0a2d2b0a91bc5f65ef23089fa5673982f75134
Ben Scott <ben@reload.me.uk>
2013-04-26 23:34:22 +0100

    Tidy up config, Fix highlight, Add nojekyll

--- config.rb
+++ config.rb
@@ -1,5 +1,27 @@
+###
+# Extensions
+###
+
+activate :directory_indexes
+activate :syntax
 activate :minify_html
 activate :livereload
+
+activate :blog do |blog|
+ blog.permalink = ":year/:title.html"
+  blog.sources = "articles/:year-:month-:day-:title.html"
+  blog.taglink = "tags/:tag.html"
+  blog.layout = "layouts/blog-article.erb"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
 
 activate :deploy do |deploy|
   # As this is a user page, the page info is built off the master branch
@@ -21,30 +43,15 @@ end
 # Page options, layouts, aliases and proxies
 ###
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+page "/index.xml", :layout => false
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Middleman doesn't like dotfiles so proxy to confuse it into compliance
+proxy "/.nojekyll", "/nojekyll", :layout => false, :ignore => true
+
 
 ###
 # Helpers
 ###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -57,44 +64,14 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-###
-# Blog settings
-###
-
 set :markdown_engine, :redcarpet
-set :markdown,  :fenced_code_blocks => true,
-                :autolink => true,
-                :smartypants => true
+set :markdown, {
+  :fenced_code_blocks => true,
+  :autolink => true,
+  :smartypants => true,
+  :no_intra_emphasis => true
+}
 
-activate :syntax, :linenos => 'table'
-
-# Time.zone = "UTC"
-
-activate :blog do |blog|
-  # blog.prefix = "blog"
-  blog.permalink = ":year/:title.html"
-  blog.sources = "articles/:year-:month-:day-:title.html"
-  blog.taglink = "tags/:tag.html"
-  blog.layout = "layouts/blog-article.erb"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
-
-  blog.tag_template = "blog/tag.html"
-  blog.calendar_template = "blog/calendar.html"
-
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/:num"
-end
-
-page "/index.xml", :layout => false
-
-
-activate :directory_indexes
 
 # Build-specific configuration
 configure :build do
@@ -103,16 +80,5 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+  activate :cache_buster
 end
